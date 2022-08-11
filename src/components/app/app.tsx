9234f3647f8e36880f2eddb6dfad2a528cebce34
Dmitry Yarlykov <justday@inbox.ru>
2022-08-11 18:33:42 +0300

    move app to components folder

---
+++ src/components/app/app.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './app.module.css';
+import AppHeader from '../../components/app-header/app-header';
+import BurgerIngredients from '../../components/burger-ingredients/burger-ingredients';
+import BurgerConstructor from '../../components/burger-constructor/burger-constructor';
+
+function App() {
+  return (
+    <div className="App">
+      <AppHeader />
+      <main>
+        <BurgerIngredients />
+        <BurgerConstructor />
+      </main>
+    </div>
+  );
+}
+
+export default App;
