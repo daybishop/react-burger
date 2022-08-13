6e86897214c6eece8ad38d3241a3c2a560cb9a90
Dmitry Yarlykov <justday@inbox.ru>
2022-08-13 14:32:39 +0300

    add closeOnEscapeKeyDown

--- src/components/common/modals.js
+++ src/components/common/modals.js
@@ -1,28 +1,44 @@
 import { createPortal } from "react-dom";
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './modals.module.css';
-
-const modalRoot = document.getElementById("react-modals");
+import { useEffect } from "react";
 
 const ModalOverlay = ({ children, handleClose }) => {
     return (
-        <div className={styles.modal_overlay} onClick={handleClose} onKeyDown={handleClose}>
+        <div className={styles.modal_overlay} onClick={handleClose}>
             {children}
         </div>
     )
 }
 
 export default function Modal({ children, header, handleClose }) {
-    return createPortal((
-        <ModalOverlay handleClose={handleClose}>
-            <section className={styles.modal_main}>
-                <div className={`text text_type_main-large pt-10 pb-6 ${styles.title}`}>
-                    <span>{header}</span>
-                    <CloseIcon onClick={handleClose} />
-                </div>
-                {children}
-            </section>
-        </ModalOverlay>
-    ), modalRoot
+    const modalRoot = document.getElementById("modal-root");
+
+    const closeOnEscapeKeyDown = (e) => {
+        if ((e.charCode || e.keyCode) === 27) {
+            handleClose();
+        }
+    }
+
+    useEffect(() => {
+        document.body.addEventListener('keydown', closeOnEscapeKeyDown)
+        return () => {
+            document.body.removeEventListener('keydown', closeOnEscapeKeyDown)
+        }
+    }, [])
+
+    return createPortal(
+        (
+            <ModalOverlay handleClose={handleClose}>
+                <section className={styles.modal_main} onClick={e => e.stopPropagation()}>
+                    <div className={`text text_type_main-large pt-10 pb-6 ${styles.title}`}>
+                        <span>{header}</span>
+                        <CloseIcon onClick={handleClose} />
+                    </div>
+                    {children}
+                </section>
+            </ModalOverlay >
+        ),
+        modalRoot
     );
 } 
