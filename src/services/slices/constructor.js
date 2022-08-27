fa0555993eebf2b4eabdae43f25dfb463dfeba51
Dmitry Yarlykov <justday@inbox.ru>
2022-08-27 10:18:24 +0300

    change constructor slice

--- src/services/slices/constructor.js
+++ src/services/slices/constructor.js
@@ -2,8 +2,12 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
     items: [],
+    bun: null,
+    totalPrice: 0,
     orderNumber: null,
 }
+
+const calculateTotalPrice = state => (state.bun ? 2 * state.bun.price : 0) + state.items.reduce((prev, item) => prev + item.price, 0)
 
 export const constructorSlice = createSlice({
     name: 'constructor',
@@ -11,9 +15,19 @@ export const constructorSlice = createSlice({
     reducers: {
         addItem: (state, action) => {
             state.items.push(action.payload)
+            state.totalPrice = calculateTotalPrice(state)
         },
         deleteItem: (state, action) => {
-            state.items = state.items.filter(id => id !== action.payload)
+            state.items = state.items.filter((_, index) => index !== action.payload)
+            state.totalPrice = calculateTotalPrice(state)
+        },
+        addBun: (state, action) => {
+            state.bun = action.payload
+            state.totalPrice = calculateTotalPrice(state)
+        },
+        deleteBun: state => {
+            state.bun = null
+            state.totalPrice = calculateTotalPrice(state)
         },
         setOrderNumber: (state, action) => {
             state.orderNumber = action.payload
@@ -21,6 +35,6 @@ export const constructorSlice = createSlice({
     },
 })
 
-export const { addItem, deleteItem, setOrderNumber } = constructorSlice.actions
+export const { addItem, deleteItem, addBun, deleteBun, setOrderNumber } = constructorSlice.actions
 
 export default constructorSlice.reducer
