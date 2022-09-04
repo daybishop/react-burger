01a3231116e4dbd96687795d7d7a11bbaee284df
Dmitry Yarlykov <justday@inbox.ru>
2022-09-04 21:26:38 +0300

    change header

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -2,13 +2,14 @@ import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
 import { BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './app-header.module.css';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const NavLink = ({ className, icon, title }) => {
+const NavLink = ({ className, icon, title, to }) => {
     return (
         <div className={className}>
             <span className={`text text_type_main-default ${styles.button_text}`}>
                 {icon}
-                {title}
+                <Link to={to}>{title}</Link>
             </span>
         </div>
     );
@@ -18,21 +19,23 @@ NavLink.propTypes = {
     className: PropTypes.string.isRequired,
     icon: PropTypes.objectOf(BurgerIcon),
     title: PropTypes.string.isRequired,
+    to: PropTypes.string.isRequired,
 };
 
 export default function AppHeader() {
+
     return (
         <header className={styles.header}>
             <nav className={styles.nav}>
                 <div className={styles.links}>
-                    <NavLink className={styles.nav_link_active} title="Конструктор" icon={<BurgerIcon type="primary" />} />
-                    <NavLink className={styles.nav_link} title="Лента заказов" icon={<ListIcon type="secondary" />} />
+                    <NavLink className={styles.nav_link_active} title="Конструктор" icon={<BurgerIcon type="primary" />} to='/' />
+                    <NavLink className={styles.nav_link} title="Лента заказов" icon={<ListIcon type="secondary" />} to='/profile/orders' />
                 </div>
                 <div className={styles.logo}>
                     <Logo />
                 </div>
                 <div className={styles.profile}>
-                    <NavLink className={styles.nav_link_profile} title="Личный кабинет" icon={<ProfileIcon type="secondary" />} />
+                    <NavLink className={styles.nav_link_profile} title="Личный кабинет" icon={<ProfileIcon type="secondary" />} to='/profile' />
                 </div>
             </nav>
         </header >
