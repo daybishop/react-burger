96b1720c6057d4b2141ea3481680e68a55d90185
Dmitry Yarlykov <justday@inbox.ru>
2022-09-04 21:37:06 +0300

    add selectors

--- src/services/selectors/ingredients.js
+++ src/services/selectors/ingredients.js
@@ -1,3 +1,9 @@
 export const items = state => state.ingredients.items
 export const selectedItem = state => state.ingredients.selectedItem
 export const currentTab = state => state.ingredients.currentTab
+
+export const ingredientsSelectors = {
+    items,
+    selectedItem,
+    currentTab,
+}
