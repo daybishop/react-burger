e5926f6d98815eaa6b7ba753e201af9a85bb1a40
Dmitry Yarlykov <justday@inbox.ru>
2022-09-26 21:20:44 +0300

    fix header icons state color

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -2,7 +2,7 @@ import { Logo } from '@ya.praktikum/react-developer-burger-ui-components';
 import { BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './app-header.module.css';
 import PropTypes from 'prop-types';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useRouteMatch } from 'react-router-dom';
 
 const HeaderLink = ({ children, title, to }) => {
     return (
@@ -26,15 +26,19 @@ HeaderLink.propTypes = {
 
 export default function AppHeader() {
 
+    const burgerIconType = useRouteMatch({ path: '/', exact: true }) ? 'primary' : 'secondary'
+    const listIconType = useRouteMatch({ path: '/profile/orders', exact: true }) ? 'primary' : 'secondary'
+    const profileIconType = useRouteMatch({ path: '/profile', exact: true }) ? 'primary' : 'secondary'
+
     return (
         <header className={styles.header}>
             <nav className={styles.nav}>
                 <div className={styles.links}>
                     <HeaderLink title="Конструктор" to='/'>
-                        {<BurgerIcon type="primary" />}
+                        {<BurgerIcon type={burgerIconType} />}
                     </HeaderLink>
                     <HeaderLink title="Лента заказов" to='/profile/orders' >
-                        {<ListIcon type="secondary" />}
+                        {<ListIcon type={listIconType} />}
                     </HeaderLink>
                 </div>
                 <div className={styles.logo}>
@@ -44,7 +48,7 @@ export default function AppHeader() {
                 </div>
                 <div className={styles.profile}>
                     <HeaderLink title="Личный кабинет" to='/profile' >
-                        {<ProfileIcon type="secondary" />}
+                        {<ProfileIcon type={profileIconType} />}
                     </HeaderLink>
                 </div>
             </nav>
