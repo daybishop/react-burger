3d21406e2f4267069d176a51b04dedd7bff18601
Dmitry Yarlykov <justday@inbox.ru>
2022-10-16 17:01:04 +0300

    add reducers and middleware to store

--- src/store.js
+++ src/store.js
@@ -4,18 +4,30 @@ import logger from 'redux-logger'
 import constructorReducer, { initialState } from './services/slices/constructor'
 import thunk from 'redux-thunk'
 import userReducer from './services/slices/user'
+import feedReducer, { actions as feedActions } from './services/slices/feed'
 import { loadState } from './utils/local-storage'
+import { socketMiddleware } from './services/api/wsMiddleware'
+import orderReducer, { actions as orderActions } from './services/slices/orders'
 
 
 const preloadedState = loadState() || { constructor: initialState }
 
 export const store = configureStore({
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware(thunk).concat(logger),
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware(thunk)
+            .concat(logger)
+            .concat(socketMiddleware('wss://norma.nomoreparties.space/orders/all', feedActions))
+            .concat(socketMiddleware('wss://norma.nomoreparties.space/orders', orderActions, true)),
     devTools: process.env.NODE_ENV !== 'production',
     preloadedState,
     reducer: {
         ingredients: ingredientsReducer,
         constructor: constructorReducer,
         user: userReducer,
+        feed: feedReducer,
+        orders: orderReducer
     },
 })
+
+// export type AppDispatch = typeof store.dispatch
+// export type RootState = ReturnType<typeof store.getState>
